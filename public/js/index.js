1de8c0e04e182b4efb8c029dbb21349d9e0b5e9d
nshokeen0007 <n.shokeen0007@gmail.com>
2024-04-09 21:38:52 +0530

    new for srch
    this is srch car

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,38 @@
 $(document).ready(function(){
+    // document.addEventListener('DOMContentLoaded', function() {
+    var datePicker = document.getElementById('formdate');
+    var todatePicker = document.getElementById('todate');
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+    datePicker.setAttribute('min', today);
+    todatePicker.setAttribute('min', today);
+
+
+    $(document).on('click','#SearchCar', function () {
+       var from_location = $('#LocationTag').val();
+       var to_date = $("#formdate").val() ;
+       var from_date = $("#todate").val();
     
+       if (!from_location) {
+         alert("Please select a location");
+       }else{ 
+
+        console.log(from_location,to_date,from_date)
+       }
+
+       $.ajax({
+        url:'/checkdata',
+        method:'post',
+        type:'json',
+        data:{},
+        success:function(){
+          
+        }
+       })
+    
+})
 })
